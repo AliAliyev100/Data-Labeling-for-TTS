e038cfdf2fbda12e4544ed5901fe3553a8e1882b
Your Name <you@example.com>
2023-06-14 14:23:57 +0400

    Auth & user

--- routes/files.js
+++ routes/files.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const fileController = require("../controllers/files");
+const isAuth = require("../middleware/is-auth")
 
 router.post("/add-item-text", fileController.addItemText);
 
-router.get("/label", fileController.getLabel);
 
-router.post("/gettextvalues", fileController.getTextValues);
+router.get("/gettextvalues", isAuth, fileController.getTextValues);
 
 module.exports = router;
