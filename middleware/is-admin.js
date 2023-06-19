68cbd278538f57c3c631479bab2c9b3c47816098
Your Name <you@example.com>
2023-06-19 22:04:35 +0400

    deleting logs

--- middleware/is-admin.js
+++ middleware/is-admin.js
@@ -5,10 +5,6 @@ const User = require("../models/user");
 module.exports = (req, res, next) => {
   User.findById(req.userId)
     .then((user) => {
-      console.log(user);
-      console.log("sadasdsa");
-      console.log(user.isAdmin);
-      console.log("```````````");
       if (!user) {
         return res.status(401).json({ message: "Not Authenticated" });
       }
